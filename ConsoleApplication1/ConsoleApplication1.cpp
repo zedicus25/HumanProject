a150bb99e809418f907f189ffdf4e0b6357c17ca
Alexander Tsyganok <Ciga_fu80@student.itstep.org>
2021-12-19 14:29:53 +0200

    Сalled the required methods in main

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,20 @@
 ﻿#include <iostream>
 #include "c_SchoolBoy.h"
 #include "c_Student.h"
+#include "c_Worker.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+    c_SchoolBoy schoolboy;
+    c_Student student;
+    c_Worker worker;
+
+    std::cin >> schoolboy;
+    system("cls");
+    std::cin >> student;
+    system("cls");
+    std::cin >> worker;
+    system("cls");
+    std::cout << schoolboy << "\n";
+    std::cout << student << "\n";
+    std::cout << worker << "\n";
 }
